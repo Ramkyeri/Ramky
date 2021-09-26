fa585bce1a6b5566e53b2dd3802dceb427348719
Muthusamy Ramakrishnan <70749594+Ramkyeri@users.noreply.github.com>
2021-09-26 17:55:33 +0800

    Add files via upload

---
+++ R/Ramky.R
@@ -0,0 +1,18 @@
+#'This creates histogram,boxplot and numeric summary
+#'@export
+#'@param x numeric variable
+
+Ramky<-function(x){
+  #1 row and 2 colmns
+  par(mfrow = c(1,2))
+  #Histogram
+  hist(x,col = rainbow(30))
+  #BoX plot
+  boxplot(x,col ='green')
+  par(mfrow = c(1,1))
+  #Numeric summary
+  data.frame(min = min(x),
+             median = median(x),
+             mean =mean(x),
+             max = max(x))
+}
